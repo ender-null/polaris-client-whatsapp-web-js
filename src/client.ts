124ef0fb248cc7b319d86b0c16fee05b7ed6b0dd
ender <ender@end.works>
2024-04-15 10:31:21 +0200

    use mongodb

--- src/client.ts
+++ src/client.ts
@@ -3,9 +3,10 @@ import WebSocket from 'ws';
 import { Bot } from './bot';
 import { WSMessage } from './types';
 import { catchException, logger } from './utils';
-import { Client, LocalAuth } from 'whatsapp-web.js';
+import { Client, RemoteAuth } from 'whatsapp-web.js';
 import QRCode from 'qrcode';
-//import mongoose from 'mongoose';
+import mongoose from 'mongoose';
+import { MongoStore } from 'wwebjs-mongo';
 
 let bot: Bot;
 let ws: WebSocket;
@@ -27,94 +28,93 @@ process.on('exit', () => {
   logger.warn(`Exit process`);
 });
 
-//mongoose.connect(process.env.MONGODB_URI).then(() => {
-// const store = new MongoStore({ mongoose: mongoose });
-const wwebVersion = '2.2412.54';
-const client = new Client({
-  authStrategy: new LocalAuth(),
-  /*authStrategy: new RemoteAuth({
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  const store = new MongoStore({ mongoose: mongoose });
+  const wwebVersion = '2.2412.54';
+  const client = new Client({
+    authStrategy: new RemoteAuth({
       store: store,
       backupSyncIntervalMs: 300000,
-    }),*/
-  puppeteer: {
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--unhandled-rejections=strict'],
-  },
-  webVersionCache: {
-    type: 'remote',
-    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
-  },
-});
-
-client.on('qr', async (qr_code) => {
-  const qr = await QRCode.toString(qr_code, {
-    type: 'terminal',
-    small: true,
+    }),
+    puppeteer: {
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--unhandled-rejections=strict'],
+    },
+    webVersionCache: {
+      type: 'remote',
+      remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+    },
   });
-  logger.info(`QR received:\n${qr}`);
-});
 
-client.on('ready', async () => {
-  logger.info('Client is ready!');
-  await bot.init();
-});
+  client.on('qr', async (qr_code) => {
+    const qr = await QRCode.toString(qr_code, {
+      type: 'terminal',
+      small: true,
+    });
+    logger.info(`QR received:\n${qr}`);
+  });
 
-client.on('message', async (message) => {
-  const msg = await bot.convertMessage(message);
-  const data: WSMessage = {
-    bot: bot.user.username,
-    platform: 'whatsapp',
-    type: 'message',
-    message: msg,
-  };
-  ws.send(JSON.stringify(data));
-});
+  client.on('ready', async () => {
+    logger.info('Client is ready!');
+    await bot.init();
+  });
 
-client.initialize();
+  client.on('message', async (message) => {
+    const msg = await bot.convertMessage(message);
+    const data: WSMessage = {
+      bot: bot.user.username,
+      platform: 'whatsapp',
+      type: 'message',
+      message: msg,
+    };
+    ws.send(JSON.stringify(data));
+  });
 
-ws = new WebSocket(process.env.SERVER);
-bot = new Bot(ws, client);
+  client.initialize();
 
-clearInterval(pingInterval);
-pingInterval = setInterval(() => {
-  bot.ping();
-}, 30000);
+  ws = new WebSocket(process.env.SERVER);
+  bot = new Bot(ws, client);
 
-ws.on('error', async (error: WebSocket.ErrorEvent) => {
-  if (error['code'] === 'ECONNREFUSED') {
-    logger.info(`Waiting for server to be available...`);
-  } else {
-    logger.error(error);
-  }
-});
-
-ws.on('open', async () => {
-  //await bot.init()
-});
-
-ws.on('close', async (code) => {
-  if (client.pupPage) {
-    await client.sendPresenceUnavailable();
-    await client.setStatus('Offline');
-  }
-  if (code === 1005) {
-    logger.warn(`Disconnected`);
-  } else if (code === 1006) {
-    logger.warn(`Terminated`);
-  }
   clearInterval(pingInterval);
-  process.exit();
-});
+  pingInterval = setInterval(() => {
+    bot.ping();
+  }, 30000);
 
-ws.on('message', (data: string) => {
-  try {
-    const msg = JSON.parse(data);
-    logger.info(JSON.stringify(msg, null, 4));
-    if (msg.type === 'message') {
-      bot.sendMessage(msg.message);
+  ws.on('error', async (error: WebSocket.ErrorEvent) => {
+    if (error['code'] === 'ECONNREFUSED') {
+      logger.info(`Waiting for server to be available...`);
+    } else {
+      logger.error(error);
     }
-  } catch (error) {
-    catchException(error);
-  }
+  });
+
+  ws.on('open', async () => {
+    //await bot.init()
+  });
+
+  ws.on('close', async (code) => {
+    if (client.pupPage) {
+      await client.sendPresenceUnavailable();
+      await client.setStatus('Offline');
+    }
+    if (code === 1005) {
+      logger.warn(`Disconnected`);
+    } else if (code === 1006) {
+      logger.warn(`Terminated`);
+    }
+    clearInterval(pingInterval);
+    process.exit();
+  });
+
+  ws.on('message', (data: string) => {
+    try {
+      const msg = JSON.parse(data);
+      logger.info(JSON.stringify(msg, null, 4));
+      if (msg.type === 'message') {
+        bot.sendMessage(msg.message);
+      }
+    } catch (error) {
+      catchException(error);
+    }
+  });
 });
-//});
